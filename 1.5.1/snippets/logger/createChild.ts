ebd602699efbef298a618518dcd7ffaf945527ad
Docs <docs@dummy.bot.com>
2023-02-15 11:31:51 +0000

    Deployed a9658c93 to 1.5.1 with MkDocs 1.4.2 and mike 1.1.2

--- 1.5.1/snippets/logger/createChild.ts
+++ 1.5.1/snippets/logger/createChild.ts
@@ -1,21 +1,28 @@
 import { Logger } from '@aws-lambda-powertools/logger';
 
-// With this logger, all the INFO logs will be printed
+// This logger has a service name, some persistent attributes
+// and log level set to INFO
 const logger = new Logger({
-    logLevel: 'INFO'
+  serviceName: 'serverlessAirline',
+  logLevel: 'INFO',
+  persistentLogAttributes: { 
+    aws_account_id: '123456789012',
+    aws_region: 'eu-west-1',
+  },
 });
 
-// With this logger, only the ERROR logs will be printed
+// This other logger inherits all the parent's attributes 
+// but the log level, which is now set to ERROR
 const childLogger = logger.createChild({
-    logLevel: 'ERROR'
+  logLevel: 'ERROR'
 });
 
-export const handler = async (_event: any, _context: any): Promise<void> => {
+export const handler = async (_event: unknown, _context: unknown): Promise<void> => {
 
-    logger.info('This is an INFO log, from the parent logger');
-    logger.error('This is an ERROR log, from the parent logger');
+  logger.info('This is an INFO log, from the parent logger');
+  logger.error('This is an ERROR log, from the parent logger');
     
-    childLogger.info('This is an INFO log, from the child logger');
-    childLogger.error('This is an ERROR log, from the child logger');
+  childLogger.info('This is an INFO log, from the child logger');
+  childLogger.error('This is an ERROR log, from the child logger');
 
 };
