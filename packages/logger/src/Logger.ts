26c8e58e3a431508180d7d9673f282993ef44e7b
Andrea Amorosi <dreamorosi@gmail.com>
2023-11-16 16:13:34 +0100

    feat(logger): add support for `AWS_LAMBDA_LOG_LEVEL` and `POWERTOOLS_LOG_LEVEL` (#1795)
    * feat(logger): support advanced logging
    
    * docs(logger): add alc info
    
    * feat(logger): support alc
    
    * docs: fix alc docs links
    
    * tests(logger): add unit tests for the feature
    
    * docs(logger): make POWERTOOLS_LOG_LEVEL default

--- packages/logger/src/Logger.ts
+++ packages/logger/src/Logger.ts
@@ -2,6 +2,7 @@ import { Utility } from '@aws-lambda-powertools/commons';
 import type { HandlerMethodDecorator } from '@aws-lambda-powertools/commons/types';
 import type { Context, Handler } from 'aws-lambda';
 import merge from 'lodash.merge';
+import { format } from 'node:util';
 import { Console } from 'node:console';
 import { randomInt } from 'node:crypto';
 import { EnvironmentVariablesService } from './config/EnvironmentVariablesService.js';
@@ -486,9 +487,13 @@ class Logger extends Utility implements LoggerInterface {
   /**
    * Set the log level for this Logger instance.
    *
+   * If the log level is set using AWS Lambda Advanced Logging Controls, it sets it
+   * instead of the given log level to avoid data loss.
+   *
    * @param logLevel The log level to set, i.e. `error`, `warn`, `info`, `debug`, etc.
    */
   public setLogLevel(logLevel: LogLevel): void {
+    if (this.awsLogLevelShortCircuit(logLevel)) return;
     if (this.isValidLogLevel(logLevel)) {
       this.logLevel = this.logLevelThresholds[logLevel];
     } else {
@@ -568,6 +573,30 @@ class Logger extends Utility implements LoggerInterface {
    */
   private addToPowertoolsLogData(attributes: Partial<PowertoolsLogData>): void {
     merge(this.powertoolsLogData, attributes);
+  }
+
+  private awsLogLevelShortCircuit(selectedLogLevel?: string): boolean {
+    const awsLogLevel = this.getEnvVarsService().getAwsLogLevel();
+    if (this.isValidLogLevel(awsLogLevel)) {
+      this.logLevel = this.logLevelThresholds[awsLogLevel];
+
+      if (
+        this.isValidLogLevel(selectedLogLevel) &&
+        this.logLevel > this.logLevelThresholds[selectedLogLevel]
+      ) {
+        this.warn(
+          format(
+            `Current log level (%s) does not match AWS Lambda Advanced Logging Controls minimum log level (%s). This can lead to data loss, consider adjusting them.`,
+            selectedLogLevel,
+            awsLogLevel
+          )
+        );
+      }
+
+      return true;
+    }
+
+    return false;
   }
 
   /**
@@ -845,17 +874,21 @@ class Logger extends Utility implements LoggerInterface {
 
   /**
    * Sets the initial Logger log level based on the following order:
-   * 1. If a log level is passed to the constructor, it sets it.
-   * 2. If a log level is set via custom config service, it sets it.
-   * 3. If a log level is set via env variables, it sets it.
+   * 1. If a log level is set using AWS Lambda Advanced Logging Controls, it sets it.
+   * 2. If a log level is passed to the constructor, it sets it.
+   * 3. If a log level is set via custom config service, it sets it.
+   * 4. If a log level is set via env variables, it sets it.
    *
-   * If none of the above is true, the default log level applies (INFO).
+   * If none of the above is true, the default log level applies (`INFO`).
    *
    * @private
    * @param {LogLevel} [logLevel] - Log level passed to the constructor
    */
   private setInitialLogLevel(logLevel?: LogLevel): void {
     const constructorLogLevel = logLevel?.toUpperCase();
+
+    if (this.awsLogLevelShortCircuit(constructorLogLevel)) return;
+
     if (this.isValidLogLevel(constructorLogLevel)) {
       this.logLevel = this.logLevelThresholds[constructorLogLevel];
 
--- packages/logger/src/types/ConfigServiceInterface.ts
+++ packages/logger/src/types/ConfigServiceInterface.ts
@@ -9,6 +9,19 @@ import type { ConfigServiceInterface as ConfigServiceBaseInterface } from '@aws-
  */
 interface ConfigServiceInterface extends ConfigServiceBaseInterface {
   /**
+   * It returns the value of the `AWS_LAMBDA_LOG_LEVEL` environment variable.
+   *
+   * The `AWS_LAMBDA_LOG_LEVEL` environment variable is set by AWS Lambda when configuring
+   * the function's log level using the Advanced Logging Controls feature. This value always
+   * takes precedence over other means of configuring the log level.
+   *
+   * @note we need to map the `FATAL` log level to `CRITICAL`, see {@link https://docs.aws.amazon.com/lambda/latest/dg/configuration-logging.html#configuration-logging-log-levels AWS Lambda Log Levels}.
+   *
+   * @returns {string}
+   */
+  getAwsLogLevel(): string;
+
+  /**
    * It returns the value of the ENVIRONMENT environment variable.
    *
    * @returns {string}
@@ -23,7 +36,11 @@ interface ConfigServiceInterface extends ConfigServiceBaseInterface {
   getLogEvent(): boolean;
 
   /**
-   * It returns the value of the LOG_LEVEL environment variable.
+   * It returns the value of the `POWERTOOLS_LOG_LEVEL, or `LOG_LEVEL` (legacy) environment variables
+   * when the first one is not set.
+   *
+   * @note The `LOG_LEVEL` environment variable is considered legacy and will be removed in a future release.
+   * @note The `AWS_LAMBDA_LOG_LEVEL` environment variable always takes precedence over the ones above.
    *
    * @returns {string}
    */
